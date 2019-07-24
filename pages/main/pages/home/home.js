d580aa354b65835b25f259bda51c8c85fb3201f8
qq22711289 <weixiangyingln@163.com>
2019-07-24 15:07:01 +0800

    首页去掉同学圈

--- pages/main/pages/home/home.js
+++ pages/main/pages/home/home.js
@@ -24,6 +24,7 @@ Page({
         showEmpty: false,
         renewMask:true,//续费弹窗
         renew_content:'',
+      number:0
 
         
     },
@@ -90,6 +91,7 @@ Page({
                 realHeight2: res[0].height
             })
         })
+        this.getlist()
         
     },
     tz_little: function () {
@@ -104,6 +106,25 @@ Page({
         })
 
     },
+  getlist: function () {
+    var that = this
+    wx.request({
+      url: 'http://social.ajihua888.com/v14/public/test',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      method: 'POST',
+      data: {
+        "app_source_type": app.app_source_type,
+      },
+      success: function (res) {
+        console.log(res.data.data[0].test)
+        that.setData({
+          number: res.data.data[0].test
+        })
+      }
+    })
+  },
     onShow: function() {
         if (app.userInfo.register_community_name) {
             this.setData({
