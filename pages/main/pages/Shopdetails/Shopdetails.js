c06cc260a7904a6f56d904589648dc2e64c7a766
zhangyk <admin@uimake.cn>
2019-08-06 11:26:13 +0800

    商城固定id

--- pages/main/pages/Shopdetails/Shopdetails.js
+++ pages/main/pages/Shopdetails/Shopdetails.js
@@ -204,7 +204,7 @@ Page({
       params: {
         "mobile": app.userInfo.mobile,
         "token": app.userInfo.token,
-        "bid": 1,
+        "bid": 55,
 
       },
       success: function (res) {
@@ -304,7 +304,7 @@ Page({
       params: {
         "mobile": app.userInfo.mobile,
         "token": app.userInfo.token,
-        "bid": 1,
+        "bid": 55,
         "page": page
 
 
@@ -358,7 +358,7 @@ Page({
       params: {
         "mobile": app.userInfo.mobile,
         "token": app.userInfo.token,
-        "bid": 1,
+        "bid": 55,
         "cb_id": dataid
 
       },
